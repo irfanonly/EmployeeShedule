e3aee8eac57c561a86e5678cfdc43c5e1f9d9537
Irfan <mamirfan92@gmail.com>
2017-08-02 19:43:50 +0530

    filter

--- EmployeeShedule/Controllers/EmployeeController.cs
+++ EmployeeShedule/Controllers/EmployeeController.cs
@@ -11,12 +11,37 @@ namespace SafeMode.Controllers
         employeeSheduleEntities db = new employeeSheduleEntities();
 
         // GET: Employee
-        public ActionResult Index()
+        public ActionResult Index(int? teamid, string firstname, string lastname, string email, string mobilenumber)
         {
             var teams = db.TEAMs;
             ViewBag.teamid = new SelectList(teams, "ID", "TeamName");
 
-            var emp = db.EMPLOYEEs;
+            var emp = db.EMPLOYEEs.AsQueryable();
+
+            if (teamid != null)
+            {
+                emp = emp.Where(x => x.TeamID == teamid.Value);
+            }
+
+            if (!String.IsNullOrEmpty(firstname))
+            {
+                emp = emp.Where(x => x.FirstName.Contains(firstname));
+            }
+
+            if (!String.IsNullOrEmpty(lastname))
+            {
+                emp = emp.Where(x => x.LastName.Contains(lastname));
+            }
+
+            if (!String.IsNullOrEmpty(email))
+            {
+                emp = emp.Where(x => x.Email.Contains(email));
+            }
+
+            if (!String.IsNullOrEmpty(mobilenumber))
+            {
+                emp = emp.Where(x => x.MobileNo.Contains(mobilenumber));
+            }
 
 
             return View(emp);
